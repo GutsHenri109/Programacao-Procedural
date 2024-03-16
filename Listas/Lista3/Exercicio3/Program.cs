1d3dfeecd4f12ce57d53ce81e3a92ef01e9e9244
Ariana Santos <gutoaraujohenrique@gmail.com>
2024-03-15 23:47:41 -0300

    Lacos de repeticao aninhados

--- Listas/Lista3/Exercicio3/Program.cs
+++ Listas/Lista3/Exercicio3/Program.cs
@@ -1,39 +1,19 @@
 ﻿using System;
-
+//Crie um programa que receba o peso e a altura de uma pessoa, calcule e exiba seu IMC. A formula para calculo do IMC é
+//imc = peso / (alltura * altura)
 class Program{
+
     static void Main (string[]args){
 
-        string usuarioCorreto  = "admin";
-        string senhaCorreta = "123senha";
-        int numeroTentativas = 3;
+        System.Console.WriteLine("Digite seu peso:");
+        double peso = double.Parse (Console.ReadLine());
+        System.Console.WriteLine("Digite sua altura:");
+        double altura = double.Parse (Console.ReadLine());
 
-        do
-        {
-            System.Console.WriteLine("Digite seu nome de Usuario: ");
-            string nomeUsuario = Console.ReadLine();
-            System.Console.WriteLine("Digite sua senha: ");
-            string senha = Console.ReadLine();
+        double calculoImc = peso / (altura * altura);
+        System.Console.WriteLine("Seu IMC eh: " + calculoImc );
 
-            if (nomeUsuario == usuarioCorreto && senha ==  senhaCorreta){
-                System.Console.WriteLine("Login realizado com sucesso");
-                break;
-            }
-            else
-            {
-                numeroTentativas--;
-                if(numeroTentativas > 0)
-                {
-                    System.Console.WriteLine("Usuario ou senha incorretos");
-                }
-                else
-                {
-                    System.Console.WriteLine("Sua conta foi bloqueada");
-                }
 
-            }
-        
 
-        }while(numeroTentativas > 0);
-        
     }
 }
---
+++ Listas/Lista4/Exercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+      for (int contador = 1; contador <= 10; contador++)
+      {
+        for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+        {
+            System.Console.WriteLine($"{contador} X {multiplicador} = {contador * multiplicador}"); 
+            System.Console.WriteLine(); //
+        }
+      }
+      
+    }
+} 

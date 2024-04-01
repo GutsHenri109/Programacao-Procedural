7cf9b2bae380c6f0f85eb8e01714ad32bb374b69
Ariana Santos <gutoaraujohenrique@gmail.com>
2024-04-01 00:06:07 -0300

    Lista 6 - Vetores e Matrizes

---
+++ Listas/Lista6/Exercicio1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] nomes = new string[10];
+        System.Console.WriteLine("Digite os 10 nomes:");
+        for (int i = 0; i < 10; i++)
+        {
+            System.Console.Write($"Nome {i + 1}: ");
+            nomes[i] = Console.ReadLine();
+        }
+        System.Console.Write("Digite o nome a ser buscado: ");
+        string nomeBusca = Console.ReadLine();
+        bool encontrado = false;
+        for (int i = 0; i < 10; i++)
+        {
+            if (nomes[i] == nomeBusca)
+            {
+                encontrado = true;
+                break;
+            }
+        }
+        if (encontrado)
+        {
+            Console.WriteLine("ACHEI");
+        }
+        else
+        {
+            Console.WriteLine("NAO ACHEI");
+        }
+    }
+}
---
+++ Listas/Lista6/Exercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] nomes = new string[10];
+        Console.WriteLine("Digite os 10 nomes:");
+        for (int contador = 0; contador < 10; contador++)
+        {
+            Console.Write($"Nome {contador + 1}: ");
+            nomes[contador] = Console.ReadLine();
+        }
+        Console.WriteLine("\nNomes nos índices pares (do último para o primeiro):");
+        for (int contador = 8; contador >= 0; contador -= 2)
+        {
+            Console.WriteLine(nomes[contador]);
+        }
+    }
+}

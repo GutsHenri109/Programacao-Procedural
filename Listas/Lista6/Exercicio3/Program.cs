373bda6206781b4b10df94b021868fae0815d9d0
Ariana Santos <gutoaraujohenrique@gmail.com>
2024-03-31 23:58:31 -0300

    Lista 6 - Vetores e Matrizes

---
+++ Listas/Lista6/Exercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matriz = new int[3, 3];
+        Console.WriteLine("Digite os 9 números inteiros:");
+        for (int linha = 0; linha < 3; linha++)
+        {
+            for (int coluna = 0; coluna < 3; coluna++)
+            {
+                Console.Write($"Número na posição [{linha},{coluna}]: ");
+                matriz[linha, coluna] = int.Parse(Console.ReadLine());
+            }
+        }
+        int somaDiagonal = 0;
+        for (int linha = 0; linha < 3; linha++)
+        {
+            somaDiagonal += matriz[linha, linha];
+        }
+        Console.WriteLine($"\nA soma dos valores da diagonal é: {somaDiagonal}");
+    }
+}
